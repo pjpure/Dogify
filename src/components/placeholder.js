47d5aa250dd10a5ec8720cd7695564deb476f12f
pjpure <pure254326@gmail.com>
2021-10-25 22:42:22 +0700

    set center

--- src/components/placeholder.js
+++ src/components/placeholder.js
@@ -8,20 +8,21 @@ const Placeholder = ({ getInputProps, getRootProps, error, touched }) => (
   >
     <div className="border_pointer">
       <input {...getInputProps()} />
-      {/* <MdCloudUpload style={{ fontSize: 50, paddingTop: 40 }} /> */}
-      <h3 style={{ lineHeight: "10pt", marginTop: "15%" }}>Drag & drop your</h3>
-      <h3 style={{ lineHeight: "25pt" }}>
-        <strong style={{ color: "#ff8c5a" }}>Dog image</strong> to{" "}
-        <strong style={{ color: "#ff8c5a" }}>Dogify</strong>
-      </h3>
-      <h3 style={{ lineHeight: "15pt" }}>the dog breed</h3>
-      <p style={{ marginTop: "5vh" }}>
-        or{" "}
-        <u style={{ color: "#ff8c5a" }}>
-          <strong>browse file</strong>
-        </u>{" "}
-        on your computer
-      </p>
+      <div>
+        <h3 style={{ lineHeight: "10pt" }}>Drag & drop your</h3>
+        <h3 style={{ lineHeight: "25pt" }}>
+          <strong style={{ color: "#ff8c5a" }}>Dog image</strong> to{" "}
+          <strong style={{ color: "#ff8c5a" }}>Dogify</strong>
+        </h3>
+        <h3 style={{ lineHeight: "15pt" }}>the dog breed</h3>
+        <p style={{ marginTop: "5vh" }}>
+          or{" "}
+          <u style={{ color: "#ff8c5a" }}>
+            <strong>browse file</strong>
+          </u>{" "}
+          on your computer
+        </p>
+      </div>
     </div>
   </div>
 );
